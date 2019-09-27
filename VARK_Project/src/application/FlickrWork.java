32ae5a7551ab72d0e7a8ac607d4fdba273da6f82
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-09-27 23:16:48 +1200

    Added commenting to FlickrWork class

--- VARK_Project/src/application/FlickrWork.java
+++ VARK_Project/src/application/FlickrWork.java
@@ -16,6 +16,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
+/**
+ * Retrieving and downloading required images from Flickr is all done in this class.
+ * It is done in the background, as it is a heavily computational task.
+ */
 public class FlickrWork extends Task<String> {
     private String _term;
     private int _num;
@@ -29,8 +33,6 @@ public class FlickrWork extends Task<String> {
     @Override
     protected String call() throws Exception {
         // https://www.flickr.com/search/?text=_term;
-        /*getHTML();
-        downloadImgs();*/
         try {
             getPhotos();
         } catch (RuntimeException e){
@@ -40,6 +42,12 @@ public class FlickrWork extends Task<String> {
         return Integer.toString(_num);
     }
 
+    /**
+     * Retrieve the specified API keys from the config folder.
+     * @param key
+     * @return String API key
+     * @throws IOException
+     */
     private String getAPIKey(String key) throws IOException {
         String config = PathCD.getPathInstance().getPath().substring(0, PathCD.getPathInstance().getPath().indexOf("/out"))
                 + "/src/application/config/flickr-api-key.txt";
@@ -51,7 +59,7 @@ public class FlickrWork extends Task<String> {
 
             String line;
             while ( (line = br.readLine()) != null ) {
-                if (line.trim().startsWith(key)) {
+                if (line.trim().startsWith(key)) { //Read in the necessary line as requested by the type of key in the input.
                     br.close();
                     return line.substring(line.indexOf("=")+1).trim();
                 }
@@ -64,6 +72,9 @@ public class FlickrWork extends Task<String> {
         return null;
     }
 
+    /**
+     * Retrieve and downloaded the photos from Flickr, through the Flickr API
+     */
     private void getPhotos(){
         try {
             String apiKey = getAPIKey("apikey");
@@ -75,13 +86,13 @@ public class FlickrWork extends Task<String> {
             int resultsPerPage = _num;
             int page = 0;
 
-            PhotosInterface photos = flickr.getPhotosInterface();
-            SearchParameters params = new SearchParameters();
+            PhotosInterface photos = flickr.getPhotosInterface(); //Get Flickr interface
+            SearchParameters params = new SearchParameters(); //Set paramaters
             params.setSort(SearchParameters.RELEVANCE);
             params.setMedia("photos");
             params.setText(query);
 
-            PhotoList<Photo> results = photos.search(params, resultsPerPage, page);
+            PhotoList<Photo> results = photos.search(params, resultsPerPage, page); //Look for photos
             System.out.println("Retrieving " + results.size()+ " results");
 
             int count = 1;
@@ -90,7 +101,7 @@ public class FlickrWork extends Task<String> {
                     BufferedImage image = photos.getImage(photo, Size.LARGE);
                     String filename = "img" + Integer.toString(count) + ".jpg";
                     File outputfile = new File(PathCD.getPathInstance().getPath() + "/mydir/extra",filename);
-                    ImageIO.write(image, "jpg", outputfile);
+                    ImageIO.write(image, "jpg", outputfile); //Download the image
                     System.out.println("Downloaded "+filename);
 
                     count++;
@@ -99,7 +110,7 @@ public class FlickrWork extends Task<String> {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace(); //TODO throw exception if cannot find images for the specific term or not enough images?
+            e.printStackTrace(); //throw exception if no images were found for such term.
             throw new RuntimeException("No Flickr results for " + _term + ". Creating a blue video instead...");
         }
 
