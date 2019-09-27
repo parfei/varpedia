151c0267c83e6bfc5d1d7a4f84caed221a9ea1c6
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-09-27 17:03:08 +1200

    Changed method of retrieving images to using Flickr API

--- VARK_Project/src/application/FlickrWork.java
+++ VARK_Project/src/application/FlickrWork.java
@@ -1,7 +1,13 @@
 package application;
 
+import com.flickr4java.flickr.Flickr;
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.*;
 import javafx.concurrent.Task;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -23,17 +29,82 @@ public class FlickrWork extends Task<String> {
     @Override
     protected String call() throws Exception {
         // https://www.flickr.com/search/?text=_term;
-        getHTML();
-        downloadImgs();
+        /*getHTML();
+        downloadImgs();*/
+
+        getPhotos();
 
         System.out.println("flickr done");
         return null;
     }
 
+    private String getAPIKey(String key) throws IOException {
+        String config = PathCD.getPathInstance().getPath() + Main.class.getProtectionDomain().getCodeSource().getLocation() + "/config/flickr-api-key.txt";
+        File file = new File(config);
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ( (line = br.readLine()) != null ) {
+                if (line.trim().startsWith(key)) {
+                    br.close();
+                    return line.substring(line.indexOf("=")+1).trim();
+                }
+            }
+            br.close();
+            throw new RuntimeException("Couldn't find " + key +" in config file "+file.getName());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void getPhotos(){
+        try {
+            String apiKey = getAPIKey("apikey");
+            String sharedSecret = getAPIKey("secret");
+
+            Flickr flickr = new Flickr(apiKey, sharedSecret, new REST());
+
+            String query = _term;
+            int resultsPerPage = _num;
+            int page = 0;
+
+            PhotosInterface photos = flickr.getPhotosInterface();
+            SearchParameters params = new SearchParameters();
+            params.setSort(SearchParameters.RELEVANCE);
+            params.setMedia("photos");
+            params.setText(query);
+
+            PhotoList<Photo> results = photos.search(params, resultsPerPage, page);
+            System.out.println("Retrieving " + results.size()+ " results");
+
+            for (Photo photo: results) {
+                try {
+                    BufferedImage image = photos.getImage(photo, Size.LARGE);
+                    String filename = query.trim().replace(' ', '-')+"-"+System.currentTimeMillis()+"-"+photo.getId()+".jpg";
+                    File outputfile = new File(PathCD.getPathInstance().getPath() + "/extra",filename);
+                    ImageIO.write(image, "jpg", outputfile);
+                    System.out.println("Downloaded "+filename);
+                } catch (FlickrException fe) {
+                    System.err.println("Ignoring image " +photo.getId() +": "+ fe.getMessage());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\nDone");
+    }
+
+
     /**
      * Get the HTML from the page to search images for.
      * CONSIDER PAGES WHICH DON'T HAVE IMAGES FOR IT?
      */
+    /*
     private void getHTML() throws IOException {
         try {
             URL url = new URL("https://www.flickr.com/search/?text=" + _term);
@@ -54,7 +125,9 @@ public class FlickrWork extends Task<String> {
             throw new IOException("Images cannot be found. Replacing with a blue background instead."); //TODO implement this.
         }
     }
+    */
 
+    /*
     private void downloadImgs() throws IOException {
         String link;
         String trueLink;
@@ -83,4 +156,5 @@ public class FlickrWork extends Task<String> {
            i++;
        }
     }
+    */
 }
