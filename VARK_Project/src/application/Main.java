7ce688bbe811b493aade7ae931375d4db344554a
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-09-27 23:01:42 +1200

    Deleted old comments when setting up video creation in FlickrWork

--- VARK_Project/src/application/Main.java
+++ VARK_Project/src/application/Main.java
@@ -27,8 +27,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 450, 300));
         primaryStage.show();
 
-        /*ExecutorService team = Executors.newSingleThreadExecutor();
-        team.submit(new FlickrWork("apple", "4"));*/
     }
 
     /**
