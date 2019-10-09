dbbc706453cd40cdfaf28cb52cb17c544e092334
JIgao-Zeng <jzen557@aucklanduni.ac.nz>
2019-10-09 13:31:00 +1300

    remove the comments

--- VARK_Project/src/application/controllers/MainController.java
+++ VARK_Project/src/application/controllers/MainController.java
@@ -19,7 +19,6 @@ public class MainController {
     private ChangeScene _changeSceneObject=new ChangeScene();
 
 
-    //private MainController controller = new MainController();
 
     /**
      * When this method is called, it will change the Scene to CreateView
@@ -30,30 +29,12 @@ public class MainController {
     @FXML
     public void create(ActionEvent event) throws IOException {
         _changeSceneObject.changeScene(event,"resources/Creation.fxml","Creation");
-
-       /* Parent createViewParent = FXMLLoader.load(Main.class.getResource("resources/Creation.fxml"));
-        Scene createViewScene = new Scene(createViewParent);
-        // gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setTitle("Creation");
-        window.setScene(createViewScene);
-        window.show();*/
-
     }
     @FXML
     public void view(ActionEvent event)throws IOException{
         _changeSceneObject.changeScene(event,"resources/View.fxml","View Menu");
-
     }
-    /*@FXML
-    public void play(ActionEvent event)throws IOException{
 
-        this.view(event);
-    }
-    @FXML
-    public void delete(ActionEvent event)throws IOException{
-        this.view(event);
-    }*/
 
 
 
