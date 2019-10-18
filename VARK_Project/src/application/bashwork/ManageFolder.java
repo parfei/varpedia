df4fbb7558548214bf25d43ad9a8c9a4819b1da0
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-10-19 00:08:47 +1300

    Fixed creation of folders initially bug

--- VARK_Project/src/application/bashwork/ManageFolder.java
+++ VARK_Project/src/application/bashwork/ManageFolder.java
@@ -1,15 +1,7 @@
 package application.bashwork;
 
 import application.PathCD;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
-
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ManageFolder {
 
@@ -20,7 +12,7 @@ public class ManageFolder {
         ArrayList<String> list = output.bash("[ -e \"" + path + "/mydir\" ]");
         if (list.isEmpty()){
         } else if (list.get(0).equals("1")) {
-            output.bash("mkdir -p \"" + path + "/mydir/extra/\" ; mkdir \"" + path + "/mydir/creations/favourites\"; ");
+            output.bash("mkdir -p \"" + path + "/mydir/extra/\" ; mkdir -p \"" + path + "/mydir/creations/favourites\"");
         }
     }
 
