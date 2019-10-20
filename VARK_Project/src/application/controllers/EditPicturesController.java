9cc0e14207b89550f5a7e1c117c70bcd68ab976e
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-10-21 03:21:41 +1300

    Added editpictures controller and elements on the scene

---
+++ VARK_Project/src/application/controllers/EditPicturesController.java
@@ -0,0 +1,19 @@
+package application.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
+public class EditPicturesController {
+    @FXML private HBox downloading;
+
+    @FXML
+    public void backToMainMenu(){
+
+    }
+
+    @FXML
+    public void toFinalScene(){
+
+    }
+
+}
