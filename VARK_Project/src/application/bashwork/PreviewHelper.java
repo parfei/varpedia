ce6419848880b3ae115cb8b8fd98a2ac657f4927
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-10-22 00:48:52 +1300

    Moved anon class to separate file v2

---
+++ VARK_Project/src/application/bashwork/PreviewHelper.java
@@ -0,0 +1,49 @@
+package application.bashwork;
+
+import application.values.PathIs;
+import javafx.concurrent.Task;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PreviewHelper extends Task<Integer> {
+        private String _voice;
+        private String _text;
+
+        public PreviewHelper(String voice, String text){
+            _text = text.replaceAll("[\\[\\](){}']",""); // remove the text in brackets to make it readable
+            _voice = voice;
+        }
+
+        @Override
+        protected Integer call() throws Exception {
+            FileWriter writer = null;
+            String cmd = "";
+
+            if (_voice.equals("default_voice")){
+                writer=new FileWriter(PathIs.TEMP + "/" + _voice);
+                writer.write("(voice_kal_diphone)"+"\n"+"(SayText" + " "+"\""+_text +"\"" + ")");
+                cmd = "festival -b \"" + PathIs.TEMP + "/default_voice\"";
+                System.out.println(cmd);
+            } else if (_voice.equals("male_voice")){
+                writer=new FileWriter(PathIs.TEMP + "/" + _voice);
+                writer.write("(voice_akl_nz_jdt_diphone)"+"\n"+"(SayText" + " "+"\""+_text+"\"" + ")");
+                cmd = "festival -b \"" + PathIs.TEMP + "/male_voice\"";
+            } else if (_voice.equals("female_voice")){
+                writer=new FileWriter(PathIs.TEMP + "/female_voice.scm");
+                writer.write("(voice_akl_nz_cw_cg_cg)"+"\n"+"(SayText" + " "+"\""+_text+"\"" + ")");
+                cmd = "festival -b \"" + PathIs.TEMP + "/female_voice.scm\"";
+            }
+            writer.close();
+
+            Process process = null;
+            ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
+            try {
+                process = pb.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return process.waitFor();
+        }
+}
--- VARK_Project/src/application/controllers/EditTextController.java
+++ VARK_Project/src/application/controllers/EditTextController.java
@@ -5,6 +5,7 @@ import application.Main;
 import application.PathCD;
 import application.bashwork.BashCommand;
 import application.bashwork.ManageFolder;
+import application.bashwork.PreviewHelper;
 import application.bashwork.SaveHelper;
 import application.values.PathIs;
 import application.values.SceneFXML;
@@ -112,49 +113,6 @@ public class EditTextController {
         return true;
     }
 
-    private class PreviewHelper extends Task<Integer> {
-        private String _voice;
-        private String _textWithoutBrackets;
-
-        public PreviewHelper(String voice){
-            _selectedText = textArea.getSelectedText();
-            _textWithoutBrackets = _selectedText.replaceAll("[\\[\\](){}']",""); // remove the text in brackets to make it readable
-            _voice = voice;
-        }
-
-        @Override
-        protected Integer call() throws Exception {
-            FileWriter writer = null;
-            String cmd = "";
-
-            if (_voice.equals("default_voice")){
-                writer=new FileWriter(PathIs.TEMP + "/" + _voice);
-                writer.write("(voice_kal_diphone)"+"\n"+"(SayText" + " "+"\""+_selectedText +"\"" + ")");
-                cmd = "festival -b \"" + PathIs.TEMP + "/default_voice\"";
-                System.out.println(cmd);
-            } else if (_voice.equals("male_voice")){
-                writer=new FileWriter(PathIs.TEMP + "/" + _voice);
-                writer.write("(voice_akl_nz_jdt_diphone)"+"\n"+"(SayText" + " "+"\""+_textWithoutBrackets+"\"" + ")");
-                cmd = "festival -b \"" + PathIs.TEMP + "/male_voice\"";
-            } else if (_voice.equals("female_voice")){
-                writer=new FileWriter(PathIs.TEMP + "/female_voice.scm");
-                writer.write("(voice_akl_nz_cw_cg_cg)"+"\n"+"(SayText" + " "+"\""+_textWithoutBrackets+"\"" + ")");
-                cmd = "festival -b \"" + PathIs.TEMP + "/female_voice.scm\"";
-            }
-            writer.close();
-
-            Process process = null;
-            ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
-            try {
-                process = pb.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return process.waitFor();
-        }
-    }
-
     @FXML
     public void preview() throws IOException {
         _selectedText = textArea.getSelectedText();
@@ -164,15 +122,15 @@ public class EditTextController {
         } else {
             //String textWithoutBrackets = _selectedText.replaceAll("[\\[\\](){}']",""); // remove the text in brackets to make it readable
             if (default_voice.isSelected()){
-                team.submit(new PreviewHelper("default_voice"));
+                team.submit(new PreviewHelper("default_voice", _selectedText));
                 System.out.println("default voice");
             } else {
 
                 PreviewHelper preview = null;
                 if (male_voice.isSelected()) {
-                    preview = new PreviewHelper("male_voice");
+                    preview = new PreviewHelper("male_voice", _selectedText);
                 } else if (female_voice.isSelected()){
-                    preview = new PreviewHelper("female_voice");
+                    preview = new PreviewHelper("female_voice", _selectedText);
                 }
 
                 team.submit(preview);
@@ -187,13 +145,7 @@ public class EditTextController {
                             alert.setContentText("Sorry for the inconvenience");
                             alert.showAndWait();
 
-                            team.submit(new PreviewHelper("default_voice"));
-                            /*FileWriter newWriter=new FileWriter("default_voice");
-                            newWriter.write("(voice_kal_diphone)"+"\n"+"(SayText" + " "+"\""+_selectedText +"\"" + ")") ;
-                            newWriter.close();
-                            String useDefault="festival -b default_voice";
-                            ProcessBuilder pronounce = new ProcessBuilder("bash", "-c", useDefault);
-                            pronounce.start();*/
+                            team.submit(new PreviewHelper("default_voice", _selectedText));
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
