2a3e49c6edf5ad6cbd6df6642868f618b7175540
Jennifer Lowe <jenniferlowe7@LIVE.com>
2019-10-24 02:35:48 +1300

    Added images for main menu

--- VARK_Project/src/application/controllers/MainWindowController.java
+++ VARK_Project/src/application/controllers/MainWindowController.java
@@ -1,4 +1,16 @@
 package application.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class MainWindowController {
+    @FXML private static ImageView creatingImg;
+    @FXML private static ImageView placeholderImg;
+
+    public static void creationInProgress(Boolean inProgress){
+            creatingImg.setVisible(inProgress);
+            placeholderImg.setVisible(!inProgress);
+    }
+
+
 }
